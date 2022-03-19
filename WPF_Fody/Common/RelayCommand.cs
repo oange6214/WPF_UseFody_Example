ba3202764315e742f4f935968f5c6650ca28af2c
柏翰 林 <oange6214@gmail.com>
2022-03-19 20:30:42 +0800

    Add RelyCommand, RelyCommand<T> Update Listbox
    Signed-off-by: 柏翰 林 <oange6214@gmail.com>

---
+++ WPF_Fody/Common/RelayCommand.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPF_Fody.Common
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        public RelayCommand(Action execute)
+        {
+            _execute = execute;
+        }
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+    }
+
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+        private readonly Func<bool> _canExecute;
+
+        public RelayCommand(Action<T> execute)
+        {
+            _execute = execute;
+        }
+
+        public RelayCommand(Action<T> execute, Func<bool> canExecute)
+        {
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+    }
+}
--- WPF_Fody/ViewModels/EmployeeViewModel.cs
+++ WPF_Fody/ViewModels/EmployeeViewModel.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
+using WPF_Fody.Common;
+using WPF_Fody.Models;
 
 namespace WPF_Fody.ViewModels
 {
@@ -13,7 +17,41 @@ namespace WPF_Fody.ViewModels
 
         public EmployeeViewModel()
         {
+            Employees.Add(new EmployeeModel
+            {
+                Id = 1,
+                Name = "Jed.Lin",
+                Department = "Software"
+            });
+            Employees.Add(new EmployeeModel
+            {
+                Id = 2,
+                Name = "Frank",
+                Department = "Software"
+            });
+            Employees.Add(new EmployeeModel
+            {
+                Id = 3,
+                Name = "Vito",
+                Department = "Software"
+            });
 
+            GetEmployeeCmd = new RelayCommand<string>(GetEmployeeAction);
+        }
+
+        private void GetEmployeeAction(string name)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                MessageBox.Show($"姓名：{name}");
+            });
+        }
+
+
+        private ObservableCollection<EmployeeModel> _employees;
+        public ObservableCollection<EmployeeModel> Employees    
+        {
+            get { return _employees ?? (_employees = new ObservableCollection<EmployeeModel>()); }
         }
     }
 }
