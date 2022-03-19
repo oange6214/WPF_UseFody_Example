ef42681d2765d91f0ad2a4b922b40bbe5be10bae
柏翰 林 <oange6214@gmail.com>
2022-03-19 18:27:53 +0800

    Signed-off-by: 柏翰 林 <oange6214@gmail.com>

---
+++ WPF_Fody/RangeValidationRule.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace WPF_Fody
+{
+    public class RangeValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if(double.TryParse(value.ToString(), out double myValue))
+            {
+                if(myValue >= 0 && myValue <= 100)
+                {
+                    return new ValidationResult(true, null);
+                }
+            }
+            return new ValidationResult(false, "Input should between 0 and 100");
+        }
+    }
+}
