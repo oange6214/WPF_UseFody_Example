3622a73ad59a309c0f80ed88c3f2f07333dc5711
柏翰 林 <oange6214@gmail.com>
2022-03-19 21:51:53 +0800

    Signed-off-by: 柏翰 林 <oange6214@gmail.com>
    增加 Priority Binding 範例

---
+++ WPF_Fody/Converters/TestMultiConvert.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace WPF_Fody.Converters
+{
+    public class TestMultiConvert : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(values.Count() == 0) return string.Empty;
+
+            switch (parameter as string)
+            {
+                case "FirstLast":
+                    return $"{values[0]} {values[1]}";
+                case "LastFirst":
+                    return $"{values[1]} {values[0]}";
+                default:
+                    throw new ArgumentException($"invalid argument {parameter}");
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WPF_Fody/Models/DataModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Fody.Models
+{
+    public class DataModel
+    {
+        private string _processSomeData = "done";
+
+        public string ProcessSomeData
+        {
+            get 
+            {
+                Task.Delay(1000).Wait();
+                return _processSomeData;
+            }
+        }
+
+        private string _myData = "download data.";
+
+        public string MyData
+        {
+            get
+            {
+                Task.Delay(1000).Wait();
+                return _myData;
+            }
+        }
+    }
+}
